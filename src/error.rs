191fde73f3268b002de0b0d50faff4e2641b731b
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-11-19 19:34:32 +0300

    Moved common methods for Rx and Tx into the Transceiver struct

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use std::fmt;
+
+use crate::{DDS_NAME, LPC_NAME, PHY_NAME};
+
+#[derive(Debug)]
+pub enum Error {
+    NoSuchDevice(DevicePart),
+    NoChannelOnDevice,
+    GeneralIIOError(industrial_io::Error),
+    NoRxBuff,
+    NoTxBuff,
+}
+
+impl From<industrial_io::Error> for Error {
+    fn from(error: industrial_io::Error) -> Self {
+        Self::GeneralIIOError(error)
+    }
+}
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
+pub enum DevicePart {
+    Phy,
+    Dds,
+    Lpc,
+}
+
+impl fmt::Debug for DevicePart {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Phy => write!(f, "{}", PHY_NAME),
+            Self::Dds => write!(f, "{}", DDS_NAME),
+            Self::Lpc => write!(f, "{}", LPC_NAME),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use industrial_io as iio;
 
-use iio::{Buffer, Channel, Context, Device};
-use std::fmt;
+use iio::{Buffer, Channel as IIOChannel, Context, Device};
+use std::cell::RefCell;
+
+mod error;
+
+pub use error::{DevicePart, Error};
 
 const PHY_NAME: &str = "ad9361-phy";
 const DDS_NAME: &str = "cf-ad9361-dds-core-lpc";
@@ -10,20 +14,12 @@ const LPC_NAME: &str = "cf-ad9361-lpc";
 #[derive(Debug)]
 pub struct AD9361 {
     control_device: Device,
-    rx_device: Device,
-    tx_device: Device,
-    rx_control_channels: [Channel; 2],
-    tx_control_channels: [Channel; 2],
-    rx_lo: Channel,
-    tx_lo: Channel,
-    rx_channels: [IQChannel; 2],
-    tx_channels: [IQChannel; 2],
-    rx_buffer: Option<Buffer>,
-    tx_buffer: Option<Buffer>,
+    pub rx: RefCell<Transceiver<Rx>>,
+    pub tx: RefCell<Transceiver<Tx>>,
 }
 
 impl AD9361 {
-    pub fn new(ctx: &Context) -> Result<Self, Error> {
+    pub fn from_ctx(ctx: &Context) -> Result<Self, Error> {
         // Acquire devices
         let control_device = ctx
             .find_device(PHY_NAME)
@@ -35,24 +31,6 @@ impl AD9361 {
             .find_device(DDS_NAME)
             .ok_or(Error::NoSuchDevice(DevicePart::Dds))?;
 
-        // Acquire control channels
-        let rx_control_channels = [
-            control_device
-                .find_channel("voltage0", false)
-                .ok_or(Error::NoChannelOnDevice)?,
-            control_device
-                .find_channel("voltage1", false)
-                .ok_or(Error::NoChannelOnDevice)?,
-        ];
-        let tx_control_channels = [
-            control_device
-                .find_channel("voltage0", true)
-                .ok_or(Error::NoChannelOnDevice)?,
-            control_device
-                .find_channel("voltage1", true)
-                .ok_or(Error::NoChannelOnDevice)?,
-        ];
-
         // Acquire local oscillator channels
         let rx_lo = control_device
             .find_channel("altvoltage0", true)
@@ -63,203 +41,208 @@ impl AD9361 {
 
         // Acquire channels
         //TODO: This should be rewritten without code duplication
+        //FIXME: recheck channels
         let rx_channels = [
-            IQChannel {
-                i: rx_device
+            Channel {
+                data: IQChannel {
+                    i: rx_device
+                        .find_channel("voltage0", false)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    q: rx_device
+                        .find_channel("voltage1", false)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    direction: Rx {},
+                },
+                control: control_device
                     .find_channel("voltage0", false)
                     .ok_or(Error::NoChannelOnDevice)?,
-                q: rx_device
-                    .find_channel("voltage1", false)
-                    .ok_or(Error::NoChannelOnDevice)?,
             },
-            IQChannel {
-                i: rx_device
-                    .find_channel("voltage2", false)
-                    .ok_or(Error::NoChannelOnDevice)?,
-                q: rx_device
-                    .find_channel("voltage3", false)
+            Channel {
+                data: IQChannel {
+                    i: rx_device
+                        .find_channel("voltage2", false)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    q: rx_device
+                        .find_channel("voltage3", false)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    direction: Rx {},
+                },
+                control: control_device
+                    .find_channel("voltage1", false)
                     .ok_or(Error::NoChannelOnDevice)?,
             },
         ];
         let tx_channels = [
-            IQChannel {
-                i: tx_device
+            Channel {
+                data: IQChannel {
+                    i: tx_device
+                        .find_channel("voltage0", true)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    q: tx_device
+                        .find_channel("voltage1", true)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    direction: Tx {},
+                },
+                control: control_device
                     .find_channel("voltage0", true)
                     .ok_or(Error::NoChannelOnDevice)?,
-                q: tx_device
-                    .find_channel("voltage1", true)
-                    .ok_or(Error::NoChannelOnDevice)?,
             },
-            IQChannel {
-                i: tx_device
-                    .find_channel("voltage2", true)
-                    .ok_or(Error::NoChannelOnDevice)?,
-                q: tx_device
-                    .find_channel("voltage3", true)
+            Channel {
+                data: IQChannel {
+                    i: tx_device
+                        .find_channel("voltage2", true)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    q: tx_device
+                        .find_channel("voltage3", true)
+                        .ok_or(Error::NoChannelOnDevice)?,
+                    direction: Tx {},
+                },
+                control: control_device
+                    .find_channel("voltage1", true)
                     .ok_or(Error::NoChannelOnDevice)?,
             },
         ];
 
+        let rx = RefCell::new(Transceiver {
+            channels: rx_channels,
+            buffer: None,
+            device: rx_device,
+            lo: rx_lo,
+        });
+
+        let tx = RefCell::new(Transceiver {
+            channels: tx_channels,
+            buffer: None,
+            device: tx_device,
+            lo: tx_lo,
+        });
+
         Ok(Self {
             control_device,
-            rx_device,
-            tx_device,
-            rx_control_channels,
-            tx_control_channels,
-            rx_lo,
-            tx_lo,
-            rx_channels,
-            tx_channels,
-            rx_buffer: None,
-            tx_buffer: None,
+            tx,
+            rx,
         })
     }
+}
 
-    pub fn set_rx_rf_bandwidth(&self, chan_id: usize, bandwidth: i64) -> Result<(), Error> {
-        self.rx_control_channels[chan_id].attr_write_int("rf_bandwidth", bandwidth)?;
+#[derive(Debug)]
+struct IQChannel<T> {
+    i: IIOChannel,
+    q: IIOChannel,
+    direction: T,
+}
+
+#[derive(Debug)]
+struct Channel<T> {
+    control: IIOChannel,
+    data: IQChannel<T>,
+}
+
+// Marker structs for directioning
+#[derive(Debug)]
+pub struct Tx;
+#[derive(Debug)]
+pub struct Rx;
+
+#[derive(Debug)]
+pub struct Transceiver<T> {
+    device: Device,
+    lo: IIOChannel,
+    channels: [Channel<T>; 2],
+    buffer: Option<Buffer>,
+}
+
+impl<T> Transceiver<T> {
+    pub fn set_rf_bandwidth(&self, chan_id: usize, bandwidth: i64) -> Result<(), Error> {
+        self.channels[chan_id]
+            .control
+            .attr_write_int("rf_bandwidth", bandwidth)?;
         Ok(())
     }
 
-    pub fn set_rx_sampling_frequency(&self, chan_id: usize, samplerate: i64) -> Result<(), Error> {
-        self.rx_control_channels[chan_id].attr_write_int("sampling_frequency", samplerate)?;
+    pub fn set_sampling_frequency(&self, chan_id: usize, samplerate: i64) -> Result<(), Error> {
+        self.channels[chan_id]
+            .control
+            .attr_write_int("sampling_frequency", samplerate)?;
         Ok(())
     }
 
-    pub fn set_rx_lo(&self, freq: i64) -> Result<(), Error> {
-        self.rx_lo.attr_write_int("frequency", freq)?;
+    pub fn set_lo(&self, freq: i64) -> Result<(), Error> {
+        self.lo.attr_write_int("frequency", freq)?;
         Ok(())
     }
 
-    pub fn set_rx_port(&self, chan_id: usize, port: RxPortSelect) -> Result<(), Error> {
-        self.rx_control_channels[chan_id].attr_write_str("rf_port_select", port.to_str())?;
+    pub fn enable(&self, chan_id: usize) {
+        self.channels[chan_id].data.i.enable();
+        self.channels[chan_id].data.q.enable();
+    }
+
+    pub fn disable(&self, chan_id: usize) {
+        self.channels[chan_id].data.i.disable();
+        self.channels[chan_id].data.q.disable();
+    }
+
+    pub fn create_buffer(&mut self, sample_count: usize, cyclic: bool) -> Result<(), Error> {
+        let buffer = self.device.create_buffer(sample_count, cyclic)?;
+        self.buffer = Some(buffer);
         Ok(())
     }
 
-    pub fn rx_enable(&self, chan_id: usize) {
-        self.rx_channels[chan_id].i.enable();
-        self.rx_channels[chan_id].q.enable();
+    pub fn destroy_buffer(&mut self) {
+        self.buffer = None;
     }
+}
 
-    pub fn rx_disable(&self, chan_id: usize) {
-        self.rx_channels[chan_id].i.disable();
-        self.rx_channels[chan_id].q.disable();
-    }
-
-    pub fn create_rx_buffer(&mut self, sample_count: usize, cyclic: bool) -> Result<(), Error> {
-        let buffer = self.rx_device.create_buffer(sample_count, cyclic)?;
-        self.rx_buffer = Some(buffer);
+impl Transceiver<Rx> {
+    pub fn set_port(&self, chan_id: usize, port: RxPortSelect) -> Result<(), Error> {
+        self.channels[chan_id]
+            .control
+            .attr_write_str("rf_port_select", port.to_str())?;
         Ok(())
-    }
-
-    pub fn destroy_rx_buffer(&mut self) {
-        self.rx_buffer = None;
     }
 
     pub fn pool_samples_to_buff(&mut self) -> Result<usize, Error> {
-        let Some(buf) = &mut self.rx_buffer else {return Err(Error::NoRxBuff);};
+        let Some(buf) = &mut self.buffer else {return Err(Error::NoRxBuff);};
         let result = buf.refill()?;
         Ok(result)
     }
 
     pub fn read(&self, chan_id: usize) -> Result<Signal, Error> {
-        let Some(buf) = &self.rx_buffer else {return Err(Error::NoRxBuff);};
-        let i_channel: Vec<i16> = self.rx_channels[chan_id].i.read(&buf)?;
-        let q_channel: Vec<i16> = self.rx_channels[chan_id].q.read(&buf)?;
+        let Some(buf) = &self.buffer else {return Err(Error::NoRxBuff);};
+        let i_channel: Vec<i16> = self.channels[chan_id].data.i.read(&buf)?;
+        let q_channel: Vec<i16> = self.channels[chan_id].data.q.read(&buf)?;
         Ok(Signal {
             i_channel,
             q_channel,
         })
     }
+}
 
-    pub fn set_tx_rf_bandwidth(&self, chan_id: usize, bandwidth: i64) -> Result<(), Error> {
-        self.tx_control_channels[chan_id].attr_write_int("rf_bandwidth", bandwidth)?;
+impl Transceiver<Tx> {
+    pub fn set_port(&self, chan_id: usize, port: TxPortSelect) -> Result<(), Error> {
+        self.channels[chan_id]
+            .control
+            .attr_write_str("rf_port_select", port.to_str())?;
         Ok(())
-    }
-
-    pub fn set_tx_sampling_frequency(&self, chan_id: usize, samplerate: i64) -> Result<(), Error> {
-        self.tx_control_channels[chan_id].attr_write_int("sampling_frequency", samplerate)?;
-        Ok(())
-    }
-
-    pub fn set_tx_lo(&self, freq: i64) -> Result<(), Error> {
-        self.tx_lo.attr_write_int("frequency", freq)?;
-        Ok(())
-    }
-
-    pub fn set_tx_port(&self, chan_id: usize, port: TxPortSelect) -> Result<(), Error> {
-        self.tx_control_channels[chan_id].attr_write_str("rf_port_select", port.to_str())?;
-        Ok(())
-    }
-
-    pub fn tx_enable(&self, chan_id: usize) {
-        self.tx_channels[chan_id].i.enable();
-        self.tx_channels[chan_id].q.enable();
-    }
-
-    pub fn tx_disable(&self, chan_id: usize) {
-        self.tx_channels[chan_id].i.disable();
-        self.tx_channels[chan_id].q.disable();
-    }
-
-    pub fn create_tx_buffer(&mut self, sample_count: usize, cyclic: bool) -> Result<(), Error> {
-        let buffer = self.tx_device.create_buffer(sample_count, cyclic)?;
-        self.tx_buffer = Some(buffer);
-        Ok(())
-    }
-
-    pub fn destroy_tx_buffer(&mut self) {
-        self.tx_buffer = None;
     }
 
     pub fn push_samples_to_device(&mut self) -> Result<usize, Error> {
-        let Some(buf) = &mut self.tx_buffer else {return Err(Error::NoTxBuff);};
+        let Some(buf) = &mut self.buffer else {return Err(Error::NoTxBuff);};
         let result = buf.push()?;
         Ok(result)
     }
 
     pub fn write(&self, chan_id: usize, signal: &Signal) -> Result<(usize, usize), Error> {
-        let Some(buf) = &self.tx_buffer else {return Err(Error::NoTxBuff);};
-        let write_i = self.tx_channels[chan_id].i.write(&buf, &signal.i_channel)?;
-        let write_q = self.tx_channels[chan_id].q.write(&buf, &signal.q_channel)?;
+        let Some(buf) = &self.buffer else {return Err(Error::NoTxBuff);};
+        let write_i = self.channels[chan_id]
+            .data
+            .i
+            .write(&buf, &signal.i_channel)?;
+        let write_q = self.channels[chan_id]
+            .data
+            .q
+            .write(&buf, &signal.q_channel)?;
         Ok((write_i, write_q))
-    }
-}
-
-#[derive(Debug)]
-struct IQChannel {
-    i: Channel,
-    q: Channel,
-}
-
-#[derive(Debug)]
-pub enum Error {
-    NoSuchDevice(DevicePart),
-    NoChannelOnDevice,
-    GeneralIIOError(industrial_io::Error),
-    NoRxBuff,
-    NoTxBuff,
-}
-
-impl From<industrial_io::Error> for Error {
-    fn from(error: industrial_io::Error) -> Self {
-        Self::GeneralIIOError(error)
-    }
-}
-
-#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
-pub enum DevicePart {
-    Phy,
-    Dds,
-    Lpc,
-}
-
-impl fmt::Debug for DevicePart {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Phy => write!(f, "{}", PHY_NAME),
-            Self::Dds => write!(f, "{}", DDS_NAME),
-            Self::Lpc => write!(f, "{}", LPC_NAME),
-        }
     }
 }
 
@@ -270,7 +253,7 @@ pub enum TxPortSelect {
 }
 
 impl TxPortSelect {
-    fn to_str(&self) -> &'static str {
+    pub fn to_str(&self) -> &'static str {
         use TxPortSelect::*;
         match self {
             A => "A",
@@ -296,7 +279,7 @@ pub enum RxPortSelect {
 }
 
 impl RxPortSelect {
-    fn to_str(&self) -> &'static str {
+    pub fn to_str(&self) -> &'static str {
         use RxPortSelect::*;
         match self {
             ABalanced => "A_BALANCED",
@@ -327,4 +310,23 @@ pub enum GainControlMode {
 pub struct Signal {
     i_channel: Vec<i16>,
     q_channel: Vec<i16>,
+}
+
+impl Signal {
+    pub fn new(i_channel: Vec<i16>, q_channel: Vec<i16>) -> Self {
+        //TODO: this should be refactored
+        assert_eq!(i_channel.len(), q_channel.len());
+        Self {
+            i_channel,
+            q_channel,
+        }
+    }
+
+    pub fn to_channels(self) -> (Vec<i16>, Vec<i16>) {
+        let Signal {
+            i_channel,
+            q_channel,
+        } = self;
+        (i_channel, q_channel)
+    }
 }
